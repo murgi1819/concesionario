e34a0f252619afd6f42ee51117f52c75ca12603f
abasso19 <alebasaba96@gmail.com>
2019-05-20 09:14:46 +0200

    Creada la clase Trabajo

---
+++ src/Trabajo.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class Trabajo {
+	private Date fecha;
+	private double kilometraje;
+	private String incidencias;
+	
+	public double valorar() {
+		return 0;
+		
+	}
+}
