34eeec634c3a6932a6ee9bea2cf3a726b08447f4
Saguira <lauramassanet.smr@gmail.com>
2019-05-20 14:13:31 +0200

    Cambios

--- src/Vehiculo.java
+++ src/Vehiculo.java
@@ -1,6 +1,33 @@
 public class Vehiculo {
+
+	
 	
 	
 	//getter
+	public int getNumRuedas(){
+		return this.numRuedas;
+	}
+	public int getPeso(){
+		return this.peso;
+	}
+	public int getNumPlazas(){
+		return this.numPlazas;
+	}
+	public double getKilometraje(){
+		return this.kilometraje;
+	}
+	//setter
+	public void setNumRuedas (int numRuedas){
+		this.numRuedas = numRuedas;
+	}
+	public void setPeso (int peso){
+		this.peso = peso;
+	}
+	public void setNumPlazas (int numPlazas){
+		this.numPlazas = numPlazas;
+	}
+	public void setKilometraje(double kilometraje){
+		this.kilometraje = kilometraje;
+	}
 	
 }
