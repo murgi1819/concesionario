395f128f560b295bba2cb0dd4226af292b7e3cf8
mrodriguezs19 <michael198rs@gmail.com>
2019-05-24 10:30:12 +0200

    Añadido motocicleta

---
+++ src/Motocicleta.java
@@ -0,0 +1,15 @@
+
+public class Motocicleta extends Motorizado {
+	
+	String tipo;
+	boolean bloqueada;
+	
+	public Motocicleta(int cilindrada, double combustible, double capacidadDepostio, int consumo) {
+		super(cilindrada, combustible, capacidadDepostio, consumo);
+		
+	}
+	
+	public void bloquea(boolean estado) {
+		
+	}
+}
