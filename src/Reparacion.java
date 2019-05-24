b782be41099857b2f16983711dea242fcbfd3977
achaves19 <alejandrochaves1997@gmail.com>
2019-05-24 13:06:31 +0200

    Clase Reparacion

---
+++ src/Reparacion.java
@@ -0,0 +1,16 @@
+
+public class Reparacion {
+	private String sistema; 
+	
+	public Reparacion(String sistema){
+		this.sistema = sistema;
+	}
+	
+	@Override
+	public String toString() {
+		return "Reparación " + sistema;
+		
+	}
+	
+	
+}
